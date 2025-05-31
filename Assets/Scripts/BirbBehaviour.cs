5fc26e7e7136f2b6ba830361219f38e6dd0aeebd
RonyL02 <280202rony@gmail.com>
2025-05-31 14:12:32 +0300

    add birds

--- Assets/Scripts/BirbBehaviour.cs
+++ Assets/Scripts/BirbBehaviour.cs
@@ -7,12 +7,13 @@ public class BirbBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var anim = GetComponent<Animator>();
+        anim.SetBool("flying", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
